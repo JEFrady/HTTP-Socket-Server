f06c903216d6a3d5420813438678495026e8321e
JEFrady <jamie.e.halliday@gmail.com>
2018-08-09 19:48:57 -1000

    Initial test client.js

--- client.js
+++ client.js
@@ -1,2 +1,12 @@
-var sanityCheck = "sanity";
-console.log(sanityCheck);
+const net = require("net");
+
+let client = new net.Socket();
+client.connect(8080, '0.0.0.0', function() {
+    console.log('Connected');
+    client.write('Hello server! - From Client');
+});
+
+client.on('data', function(data) {
+    console.log('Received' + data);
+    client.write('Received data');
+})
