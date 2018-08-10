5f33b869e4c1b9b41f26e0047dee65c46455a91e
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-08-09 20:18:00 -1000

    added data callback function in server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const net = require("net");
 const server = net.createServer(function(socket) {
   socket.write("Server testing");
   socket.pipe(socket);
+
+  socket.on("data", function() {
+    console.log("data transmitting");
+  });
 });
 
-server.listen(8080, "0.0.0.0");
+server.listen(8080, function() {
+  console.log("server running on port 8080");
+});
